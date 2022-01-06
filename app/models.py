737eb190320b87773c571f115a7581c06d378165
DenisPil <denis.pilongery@gmail.com>
2022-01-06 14:25:05 +0100

    ajout des models ticket, review

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Ticket(models.Model):
+    book_name = models.CharField(max_length=256)
+    content = models.CharField(max_length=2048, blank=True)
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class review(models.Model):
+    book_name = models.CharField(max_length=256)
+    content = models.CharField(max_length=8192)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    advised = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
