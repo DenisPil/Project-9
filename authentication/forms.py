7e2121cfe60820548884b454513597110d7901e7
DenisPil <denis.pilongery@gmail.com>
2022-01-09 22:22:42 +0100

    ajout css pour login, signup, debut home

--- authentication/forms.py
+++ authentication/forms.py
@@ -15,3 +15,14 @@ class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'last_name')
+
+
+
+
+""" 
+
+widgets = {
+            'username': forms.TextInput(attrs={'placeholder': "Nom d'utilisateur"}),
+
+        }
+"""
